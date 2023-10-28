cf463d373cdfad7c9441622e207a6761d3bf0b12
Juan Canales <41006075+INumberJuanI@users.noreply.github.com>
2023-10-28 11:50:52 +0200

    Update index.tsx

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -9,7 +9,7 @@ const Navbar = () => {
     const changeActiveElement = (value: string) => setActiveElement(value);
 
     return (<>
-        <nav className='w-full pt-[8px] pb-[4rem] bg-[var(--clr-background)]'>
+        <nav className='w-full pt-[8px] pb-[6rem] bg-[var(--clr-background)]'>
             <div className="navigation | relative w-full sm:w-max h-[70px] px-8 bg-[linear-gradient(45deg,#2196f3,#e91e63)] mx-auto flex justify-center items-center sm:rounded-[10px]">
                 <ul className='flex'>
                     <NavItem text="Home" href="#" icon={<Home />} activeElement={activeElement} changeActiveElement={changeActiveElement} />
